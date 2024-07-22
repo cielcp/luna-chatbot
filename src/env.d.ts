759d7a30d97a8ba29e580179715137ce9bdbf52a
Ciel <cielcpark@gmail.com>
2024-07-22 12:33:09 -0400

    Fixed API key leak

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="astro/client" />
 
-GROQ_API_KEY = "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";
+SECRET_GROQ_API_KEY =
+  "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";
---
+++ src/pages/endpoint.js
@@ -0,0 +1,35 @@
+import Groq from "groq-sdk";
+//import type { ChatCompletionMessageParam } from "groq-sdk/resources/chat/completions";
+
+export const prerender = false; // Ensure this endpoint is server-rendered on demand
+
+async function getGroqChatCompletion(chatHistory, apiKey) {
+  const groq = new Groq({ apiKey });
+
+  try {
+    return await groq.chat.completions.create({
+      messages: chatHistory,
+      model: "llama3-70b-8192",
+      temperature: 0.5,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      choices: [{ message: { content: "Sorry, something went wrong." } }],
+    };
+  }
+}
+
+export async function POST({ request }) {
+  const { chatHistory } = await request.json();
+  const apiKey = import.meta.env.SECRET_GROQ_API_KEY;
+
+  try {
+    const chatCompletion = await getGroqChatCompletion(chatHistory, apiKey);
+    return new Response(JSON.stringify(chatCompletion), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response("Server error", { status: 500 });
+  }
+}
