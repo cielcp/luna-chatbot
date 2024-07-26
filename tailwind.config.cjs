e48b8bc114218a6dbeb8c48d9c11fcb6d2502b21
Ciel <cielcpark@gmail.com>
2024-07-26 16:07:23 -0400

    [#3] Added webfont, logo, input component, bg color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+      },
+      colors: {
+        "off-white": "#F2F2F2",
+      },
+    },
+  },
+  plugins: [],
 };
