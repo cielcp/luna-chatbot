e718c7105e25dfad6bfd087f3d106e2720137607
cielcp <cielcpark@gmail.com>
2024-08-08 03:44:29 -0400

    [#8] Dark background, rebrand input and message bubbles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,18 @@
 module.exports = {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
       colors: {
-        "off-white": "#F2F2F2",
+        "off-white": "#F3F3F3",
+        "light-blue": "#D0E1EB",
+        "light-gray": "#6F6F6F",
+        "medium-gray": "#454545",
+        "dark-gray": "#555555",
       },
     },
   },
